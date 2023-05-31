fb005b4bc7ed5a8ece663ff3f126c438520e0374
thumutejaswini <thumutejaswini@gmail.com>
2023-05-31 22:59:56 +0530

    first commit

---
+++ registration.js
@@ -0,0 +1,7 @@
+<!-- Javascript to initialize the code list -->
+    <script>
+    var input = document.querySelector("#phone");
+    window.intlTelInput(input, {
+     separateDialCode: true
+     });
+  </script>
